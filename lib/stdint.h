3056ecdda07dda2c74a96f064878765b9d0c0050
changshun <cs_can135188@163.com>
2024-11-24 23:14:28 +0800

    add list

--- lib/stdint.h
+++ lib/stdint.h
@@ -1,5 +1,6 @@
 #ifndef __LIB_STDINT_H
 #define __LIB_STDINT_H
+#define NULL 0
 
 typedef signed char          int8_t;
 typedef signed short         int16_t;
@@ -9,5 +10,9 @@ typedef unsigned char          uint8_t;
 typedef unsigned short         uint16_t;
 typedef unsigned int           uint32_t;
 typedef unsigned long long int uint64_t;
+
+
+typedef int8_t bool;
+
 
 #endif
